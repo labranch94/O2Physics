8c89010d2053faeac946c5274b30d51061cc23c2
labranch94 <76623894+labranch94@users.noreply.github.com>
2024-03-21 17:39:37 -0300

    Update VarManager.h
    Q and Delta R definitions

--- PWGDQ/Core/VarManager.h
+++ PWGDQ/Core/VarManager.h
@@ -555,6 +555,9 @@ class VarManager : public TObject
     kCosthetaDileptonDitrack,
     kDitrackMass,
     kDitrackPt,
+    kQ, 
+    kDeltaR1,
+    kDeltaR2,
 
     // DQ-HF correlation variables
     kMassCharmHadron,
@@ -3158,18 +3161,21 @@ void VarManager::FillDileptonTrackTrack(T1 const& dilepton, T2 const& hadron1, T
   ROOT::Math::PtEtaPhiMVector v2(hadron1.pt(), hadron1.eta(), hadron1.phi(), hadronMass1);
   ROOT::Math::PtEtaPhiMVector v3(hadron2.pt(), hadron2.eta(), hadron2.phi(), hadronMass2);
   ROOT::Math::PtEtaPhiMVector v123 = v1 + v2 + v3;
-  values[kQuadMass] = v123.M() - v1.M() + DefaultdileptonMass;
+  values[kQuadMass] = v123.M();
   values[kQuadPt] = v123.Pt();
   values[kQuadEta] = v123.Eta();
   values[kQuadPhi] = v123.Phi();
 
-  if (fgUsedVars[kCosthetaDileptonDitrack] || fgUsedVars[kPairMass] || fgUsedVars[kPairPt] || fgUsedVars[kDitrackPt] || fgUsedVars[kDitrackMass]) {
+  if (fgUsedVars[kCosthetaDileptonDitrack] || fgUsedVars[kPairMass] || fgUsedVars[kPairPt] || fgUsedVars[kDitrackPt] || fgUsedVars[kDitrackMass] || fgUsedVars[kQ] || fgUsedVars[kDeltaR1] || fgUsedVars[kDeltaR2]) {
     ROOT::Math::PtEtaPhiMVector v23 = v2 + v3;
     values[kPairMass] = v1.M();
     values[kPairPt] = v1.Pt();
     values[kDitrackMass] = v23.M();
     values[kDitrackPt] = v23.Pt();
     values[kCosthetaDileptonDitrack] = (v1.Px() * v123.Px() + v1.Py() * v123.Py() + v1.Pz() * v123.Pz()) / (v1.P() * v123.P());
+    values[kQ] = v123.M() - v1.M() - v23.M();
+    values[kDeltaR1] = sqrt(pow(v1.Eta() - v2.Eta(), 2) + pow(v1.Phi() - v2.Phi(), 2));
+    values[kDeltaR2] = sqrt(pow(v1.Eta() - v3.Eta(), 2) + pow(v1.Phi() - v3.Phi(), 2));
   }
 }
 
