2d4218b7c487eb71b17c84be0d2b49d138890176
labranch94 <76623894+labranch94@users.noreply.github.com>
2024-03-25 15:53:56 -0300

    Update HistogramsLibrary.cxx
    Add Dipion Mass versus X(3872) Mass

--- PWGDQ/Core/HistogramsLibrary.cxx
+++ PWGDQ/Core/HistogramsLibrary.cxx
@@ -1067,6 +1067,8 @@ void o2::aod::dqhistograms::DefineHistograms(HistogramManager* hm, const char* h
       hm->AddHistogram(histClass, "hMass_defaultDileptonMass_DeltaR1_X3872", "", false, 1000, 3.0, 5.0, VarManager::kQuadDefaultDileptonMass, 100, 0.0, 10.0, VarManager::kDeltaR1);
       hm->AddHistogram(histClass, "hMass_DeltaR2_X3872", "", false, 1000, 3.0, 5.0, VarManager::kQuadMass, 100, 0.0, 10.0, VarManager::kDeltaR2);
       hm->AddHistogram(histClass, "hMass_defaultDileptonMass_DeltaR2_X3872", "", false, 1000, 3.0, 5.0, VarManager::kQuadDefaultDileptonMass, 100, 0.0, 10.0, VarManager::kDeltaR2);
+      hm->AddHistogram(histClass, "hMass_X3872_MassDihadron", "", false, 1000, 3.0, 5.0, VarManager::kQuadMass, 150, 0.0, 3.0, VarManager::kDitrackMass);
+      hm->AddHistogram(histClass, "hMass_defaultDileptonMass_X3872_MassDihadron", "", false, 1000, 3.0, 5.0, VarManager::kQuadDefaultDileptonMass, 150, 0.0, 3.0, VarManager::kDitrackMass);
 =======
       hm->AddHistogram(histClass, "hRap_X3872", "", false, 1000, 0.0, 10.0, VarManager::kRap);
       hm->AddHistogram(histClass, "hMass_Rap_X3872", "", false, 1000, 3.0, 5.0, VarManager::kQuadMass, 1000, 0.0, 10.0, VarManager::kRap);
